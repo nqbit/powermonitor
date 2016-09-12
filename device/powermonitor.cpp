ddd96c688a41c0aab6ce93638c8972ea030070cf
nqbit <github@nathanielquillin.com>
2016-09-12 00:48:38 -0700

    [device] Update power monitor code.

--- device/powermonitor.cpp
+++ device/powermonitor.cpp
@@ -1,11 +1,13 @@
 #define LED D7
-#define LED_ON 0
-#define LED_OFF 1
+#define LED_ON 1
+#define LED_OFF 0
 
-#define DEBUG_POWERED_ON_DELAY_MSEC 5000
-#define DEBUG_POWERED_OFF_DELAY_SEC 60
+#define POWERED_CHECK_IN      720
+#define POWERED_ON_DELAY_MSEC 5000
+#define POWERED_OFF_DELAY_SEC 60*60
 
 bool hasCheckedIn = false;
+int counter = 0;
 
 void setup() {
   pinMode(LED, OUTPUT);
@@ -19,14 +21,29 @@ void loop() {
     if (!hasCheckedIn) {
       Particle.publish("firebase", "on", PRIVATE);
       digitalWrite(LED, LED_ON);
+      delay(100);
+      digitalWrite(LED, LED_OFF);
+      delay(100);
+      digitalWrite(LED, LED_ON);
+      delay(100);
+      digitalWrite(LED, LED_OFF);
       hasCheckedIn = true;
     } else {
+      digitalWrite(LED, LED_ON);
+      delay(100);
       digitalWrite(LED, LED_OFF);
+      counter++;
+      if (counter == POWERED_CHECK_IN) {
+        hasCheckedIn = false;
+        counter = 0;
+      }
     }
   } else {
+    hasCheckedIn = false;
+    digitalWrite(LED, LED_OFF);
     // not charging
     Particle.publish("firebase", "off", PRIVATE);
-    System.sleep(SLEEP_MODE_DEEP, DEBUG_POWERED_OFF_DELAY_SEC);
+    System.sleep(SLEEP_MODE_DEEP, POWERED_OFF_DELAY_SEC);
   }
-  delay(DEBUG_POWERED_ON_DELAY_MSEC);
+  delay(POWERED_ON_DELAY_MSEC);
 }
